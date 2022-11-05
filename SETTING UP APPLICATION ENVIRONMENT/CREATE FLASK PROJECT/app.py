c572c52d0764b1a84e953668428679ad31c9703a
Akash12601 <104090639+Akash12601@users.noreply.github.com>
2022-11-05 12:39:32 +0530

    Add files via upload

---
+++ SETTING UP APPLICATION ENVIRONMENT/CREATE FLASK PROJECT/app.py
@@ -0,0 +1,10 @@
+from CREATE FLASK PROJECT import Flask
+
+CREATE FLASK PROJECT-flask(_name_)
+
+@CREATE FLASK PROJECT.route("/")
+def home():
+return '<h1> welcome to IBM</h1>'
+
+ if __name__=='__main__':
+CREATE FLASK PROJECT.run(debug=True)
